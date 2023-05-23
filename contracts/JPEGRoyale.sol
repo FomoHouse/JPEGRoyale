f678a992c8eb1a7d12ef814cb0c38ded721b4fd9
Harvey Yorke <108807429+Yamamuchi@users.noreply.github.com>
2023-05-23 12:16:33 +0100

    Create raffle

--- contracts/JPEGRoyale.sol
+++ contracts/JPEGRoyale.sol
@@ -5,23 +5,73 @@ pragma solidity 0.8.17;
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-contract JPEGRoyale is VRFConsumerBaseV2 {
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+import "@openzeppelin/contracts/utils/math/Math.sol";
+
+contract JPEGRoyale is VRFConsumerBaseV2, AccessControl {
+
+    ///////////////////// RAFFLE /////////////////////
+
+    struct EntryPrices {
+        uint48 numEntries;
+        uint256 price;
+    }
+
+    mapping(uint256 => EntryPrices[5]) public entryPrices;
+
+    struct PlayerEntries {
+        uint256 numEntries;
+        address player;
+    }
+
+    mapping(uint256 => PlayerEntries[]) public entries;
+
+    mapping(uint256 => uint256) public sellerPrice;
+
+    struct Raffle {
+        uint48 tokenId;
+        address tokenAddress;
+        address winner;
+        address seller;
+        uint256 startTime;
+        uint256 duration;
+    }
+
+    Raffle[] public raffles;
+
+    struct RaffleInfo {
+        STATUS status;
+        uint48 maxEntriesPerUser;
+        uint256 fundsRaised;
+
+    }
+
+    RaffleInfo[] public raffleInfo;
+
+    enum STATUS {
+        CREATED,
+        STARTED,
+        ENDING, 
+        ENDED
+    }
+
+
+    ///////////////////// CHAINLINK-VRF /////////////////////
+
     event RequestSent(uint256 requestId, uint32 numWords);
     event RequestFulfilled(uint256 requestId, uint256[] randomWords);
 
     struct RequestStatus {
-        bool fulfilled; // whether the request has been successfully fulfilled
-        bool exists; // whether a requestId exists
+        bool fulfilled; 
+        bool exists; 
         uint256[] randomWords;
     }
-    mapping(uint256 => RequestStatus)
-        public s_requests; /* requestId --> requestStatus */
+    mapping(uint256 => RequestStatus) public s_requests; 
     VRFCoordinatorV2Interface COORDINATOR;
 
-    // subscriptionId for the VRF request.
     uint64 s_subscriptionId;
 
-    // 
     uint256[] public requestIds;
     uint256 public lastRequestId;
 
@@ -30,26 +80,94 @@ contract JPEGRoyale is VRFConsumerBaseV2 {
     uint16 requestConfirmations = 3;
     uint32 numWords = 1;
 
+    ///////////////////// ACCESS-CONTROL /////////////////////
+
+    bytes32 public constant GAME_STARTER_ROLE = keccak256("GAME_STARTER_ROLE");
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+
+    //////////////////////////////////////////
+
     constructor(
         address _vrfCoordinator,
         uint64 _subscriptionId,
-        bytes32 _keyHash
+        bytes32 _keyHash,
+        address[] memory _gameStarter,
+        address[] memory _admin
     ) VRFConsumerBaseV2(_vrfCoordinator) {
         COORDINATOR = VRFCoordinatorV2Interface(_vrfCoordinator);
         s_subscriptionId = _subscriptionId;
         keyHash = _keyHash;
+
+        for (uint i = 0; i < _gameStarter.length; i++) {
+            _grantRole(GAME_STARTER_ROLE, _gameStarter[i]);
+        }
+
+        for (uint i = 0; i < _admin.length; i++) {
+            _grantRole(ADMIN_ROLE, _admin[i]);
+        }
     }
 
-    function setCallbackGasLimit(
-        uint32 _callbackGasLimit
-    ) public returns (uint32) {
+    function createRaffle(
+        uint48 _tokenId, 
+        address _tokenAddress, 
+        address _seller,
+        uint256 _duration, 
+        EntryPrices[] calldata _entryPrices, 
+        uint48 _maxEntriesPerUser
+    ) public onlyRole(GAME_STARTER_ROLE) returns (uint256 raffleId) {
+        require(_maxEntriesPerUser > 0, "maxEntriesPerUser is 0");
+        require(_duration > 30 minutes, "duration < 30 minutes");
+        require(_entryPrices.length > 0, "no entry prices");
+        require(_seller != address(0), "seller is 0x0");
+
+        Raffle memory raffle = Raffle({
+            tokenId: _tokenId,
+            tokenAddress: _tokenAddress,
+            winner: address(0),
+            seller: _seller,
+            startTime: block.timestamp,
+            duration: _duration
+        });
+
+        raffles.push(raffle);
+
+        RaffleInfo memory info = RaffleInfo({
+            status: STATUS.CREATED,
+            maxEntriesPerUser: _maxEntriesPerUser,
+            fundsRaised: 0
+        });
+
+        raffleInfo.push(info);
+
+        for (uint8 i = 0; i < _entryPrices.length; i++) {
+            require(_entryPrices[i].numEntries > 0, "number of entries is 0");
+            require(_entryPrices[i].price >= 0, "price is negative");
+
+            EntryPrices memory prices = EntryPrices({
+                numEntries: _entryPrices[i].numEntries,
+                price: _entryPrices[i].price
+            });
+
+            entryPrices[raffles.length - 1][i] = prices;
+        }
+
+        return raffles.length - 1;
+    }
+
+
+
+
+
+    function setCallbackGasLimit(uint32 _callbackGasLimit) public onlyRole(ADMIN_ROLE) returns (uint32 newCallbackGasLimit) {
         callbackGasLimit = _callbackGasLimit;
         return callbackGasLimit;
     }
 
-    // Assumes the subscription is funded sufficiently.
+    function addGameStarterRole(address _newGameStarter) public onlyRole(ADMIN_ROLE) {
+        _grantRole(GAME_STARTER_ROLE, _newGameStarter);
+    }
+
     function requestRandomWords() external returns (uint256 requestId) {
-        // Will revert if subscription is not set and funded.
         requestId = COORDINATOR.requestRandomWords(
             keyHash,
             s_subscriptionId,
@@ -85,4 +203,5 @@ contract JPEGRoyale is VRFConsumerBaseV2 {
         RequestStatus memory request = s_requests[_requestId];
         return (request.fulfilled, request.randomWords);
     }
+
 }
