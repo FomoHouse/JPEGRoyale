c33e92f5c788aeb3e471dbd3bc1734365106a789
Harvey Yorke <108807429+Yamamuchi@users.noreply.github.com>
2023-05-29 19:23:32 +0100

    Deploy and verify smart contract script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,56 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
 
   const arbitrumGoerliVRFCoordinator = "0x6D80646bEAdd07cE68cab36c27c626790bBcf17f";
   const arbitrumGoerliVRFSubscriptionId = "8";
   const arbitrumGoerliVRFKeyHash = "0x83d1b6e3388bed3d76426974512bb0d270e9542a765cd667242ea26c0cc0b730";
+  const gameStarter = ["0xA21B8cF5C9A5ED69b18FFB9e55d13c96A5741C16"];
+  const admin = ["0xA21B8cF5C9A5ED69b18FFB9e55d13c96A5741C16"];
+  const platformAddress = "0xA21B8cF5C9A5ED69b18FFB9e55d13c96A5741C16";
+  const gelatoProxyAddress = "0x828be1f99c1974cd46c0a92186b1cbfb3ac71610";
+  const automate = "0xa5f9b728ecEB9A1F6FCC89dcc2eFd810bA4Dec41";
+  const taskCreator = "0xA21B8cF5C9A5ED69b18FFB9e55d13c96A5741C16";
 
-  const JPEGRoyale = await hre.ethers.getContractFactory("JPEGRoyale");
-  const jpegRoyale = await JPEGRoyale.deploy(
+  const JPEGRoyaleContract = await ethers.getContractFactory("JPEGRoyale");
+  const deployedJPEGRoyaleContract = await JPEGRoyaleContract.deploy(
     arbitrumGoerliVRFCoordinator,
     arbitrumGoerliVRFSubscriptionId,
-    arbitrumGoerliVRFKeyHash
+    arbitrumGoerliVRFKeyHash,
+    gameStarter,
+    admin,
+    platformAddress,
+    gelatoProxyAddress,
+    automate,
+    taskCreator
   );
 
-  await jpegRoyale.deployed();
+  await deployedJPEGRoyaleContract.deployed();
 
-  console.log(`Deployed JPEGRoyale contract to ${jpegRoyale.address}`);
+  console.log(`Deployed JPEGRoyale contract to ${deployedJPEGRoyaleContract.address}`);
+
+  await hre.run("verify:verify", {
+    address: deployedJPEGRoyaleContract.address,
+    constructorArguments: [
+      arbitrumGoerliVRFCoordinator,
+      arbitrumGoerliVRFSubscriptionId,
+      arbitrumGoerliVRFKeyHash,
+      gameStarter,
+      admin,
+      platformAddress,
+      gelatoProxyAddress,
+      automate,
+      taskCreator
+    ]
+  });
+
+  console.log("\nSuccessfully verified JPEGRoyale contract on Ether/Arbiscan");
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+
+main() 
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
